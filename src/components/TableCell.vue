9068d82759a54ed6423d85149a559e1d75162be9
alexrod94 <alexrod94@gmail.com>
2022-06-03 10:41:25 +0200

    add colors

---
+++ src/components/TableCell.vue
@@ -0,0 +1,26 @@
+<template>
+  <td class="px-6 py-4 border-black border text-center" :class="this.color">
+    {{ items }}%
+  </td>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Number,
+  },
+  computed: {
+    color() {
+      if (this.items < 70) {
+        return "bg-red-200";
+      } else if (this.items > 70 && this.items < 85) {
+        return "bg-yellow-200";
+      } else {
+        return "bg-green-200";
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,99 +34,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            v-for="item in myData"
-          >
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap border-black border"
-            >
-              {{ item.lineName }}<br /><span class="text-small">{{
-                item.lineDesc
-              }}</span>
-            </th>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[0] < 70,
-                yellow: item.lineOEE[0] > 70 && item.lineOEE[0] < 85,
-                green: item.lineOEE[0] >= 85,
-              }"
-            >
-              {{ item.lineOEE[0] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[1] < 70,
-                yellow: item.lineOEE[1] > 70 && item.lineOEE[1] < 85,
-                green: item.lineOEE[1] >= 85,
-              }"
-            >
-              {{ item.lineOEE[1] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[2] < 70,
-                yellow: item.lineOEE[2] > 70 && item.lineOEE[2] < 85,
-                green: item.lineOEE[2] >= 85,
-              }"
-            >
-              {{ item.lineOEE[2] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[3] < 70,
-                yellow: item.lineOEE[3] > 70 && item.lineOEE[3] < 85,
-                green: item.lineOEE[3] >= 85,
-              }"
-            >
-              {{ item.lineOEE[3] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[4] < 70,
-                yellow: item.lineOEE[4] > 70 && item.lineOEE[4] < 85,
-                green: item.lineOEE[4] >= 85,
-              }"
-            >
-              {{ item.lineOEE[4] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[5] < 70,
-                yellow: item.lineOEE[5] > 70 && item.lineOEE[5] < 85,
-                green: item.lineOEE[5] >= 85,
-              }"
-            >
-              {{ item.lineOEE[5] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[6] < 70,
-                yellow: item.lineOEE[6] > 70 && item.lineOEE[6] < 85,
-                green: item.lineOEE[6] >= 85,
-              }"
-            >
-              {{ item.lineOEE[6] }}%
-            </td>
-            <td
-              class="px-6 py-4 border-black border text-center"
-              :class="{
-                red: item.lineOEE[7] < 70,
-                yellow: item.lineOEE[7] > 70 && item.lineOEE[7] < 85,
-                green: item.lineOEE[7] >= 85,
-              }"
-            >
-              {{ item.lineOEE[7] }}%
-            </td>
-          </tr>
+          <TableRow :myData="myData" />
         </tbody>
       </table>
     </div>
@@ -135,6 +43,7 @@
 
 <script>
 import fakedata from "../assets/fakedata.json";
+import TableRow from "../components/TableRow.vue";
 export default {
   data() {
     return {
@@ -145,6 +54,7 @@ export default {
     this.myData = fakedata;
     console.log(this.myData);
   },
+  components: { TableRow },
 };
 </script>
 
